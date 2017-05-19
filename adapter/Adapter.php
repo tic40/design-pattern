2897f1dff413e22a67514bd5a2803e7f27000acc
tic40 <ccpzjoh@gmail.com>
2017-05-19 09:14:55 +0900

    divide design and main

---
+++ adapter/Adapter.php
@@ -0,0 +1,38 @@
+<?php
+class Banner
+{
+    private $string;
+
+    public function __construct(string $string)
+    {
+        $this->string = $string;
+    }
+
+    public function showWithParen()
+    {
+        echo "(" . $this->string . ")" . PHP_EOL;
+    }
+
+    public function showWithAster()
+    {
+        echo "*" . $this->string . "*" . PHP_EOL;
+    }
+}
+
+interface PrintInterface
+{
+    public function printWeak();
+    public function printStrong();
+}
+
+class PrintBanner extends Banner implements PrintInterface
+{
+    public function printWeak()
+    {
+        $this->showWithParen();
+    }
+    public function printStrong()
+    {
+        $this->showWithAster();
+    }
+}
--- adapter/Main.php
+++ adapter/Main.php
@@ -1,41 +1,5 @@
 <?php
-class Banner
-{
-    private $string;
-
-    public function __construct(string $string)
-    {
-        $this->string = $string;
-    }
-
-    public function showWithParen()
-    {
-        echo "(" . $this->string . ")" . PHP_EOL;
-    }
-
-    public function showWithAster()
-    {
-        echo "*" . $this->string . "*" . PHP_EOL;
-    }
-}
-
-interface PrintInterface
-{
-    public function printWeak();
-    public function printStrong();
-}
-
-class PrintBanner extends Banner implements PrintInterface
-{
-    public function printWeak()
-    {
-        $this->showWithParen();
-    }
-    public function printStrong()
-    {
-        $this->showWithAster();
-    }
-}
+require "Iterator.php";
 
 $p = new PrintBanner("Hello");
 $p->printWeak();
