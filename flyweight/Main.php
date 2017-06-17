c5d4d0f8c94bd43cf7e9aa627a00cffa7d5c7935
tic40 <ccpzjoh@gmail.com>
2017-06-17 21:06:58 +0900

    add input message

--- flyweight/Main.php
+++ flyweight/Main.php
@@ -3,6 +3,7 @@ require_once __DIR__ . "/BigString.php";
 require_once __DIR__ . "/BigChar.php";
 require_once __DIR__ . "/BigCharFactory.php";
 
+echo 'input digits: ';
 $input = strval(trim(fgets(STDIN)));
 if (strlen($input) === 0 || ctype_digit($input) === false) {
     echo "[input error]. please input numbers like the example below." . PHP_EOL;
