4dc2988c45d6610455a62217955fc2181d1fa5c3
tic40 <ccpzjoh@gmail.com>
2017-06-24 22:19:22 +0900

    fix

--- interpreter/CommandListNode.php
+++ interpreter/CommandListNode.php
@@ -8,16 +8,18 @@ class CommandListNode extends Node
     public function parse(Context $context)
     {
         while (true) {
-            if ($context->currentToken() == null) {
+            if ($context->currentToken() === null) {
                 throw new ParseException("Missing 'end'");
-            } elseif ($context->currentToken() === "end") {
+            }
+
+            if ($context->currentToken() === "end") {
                 $context->skipToken("end");
                 break;
-            } else {
-                $commandNode = new CommandNode();
-                $commandNode->parse($context);
-                $this->list[] = $commandNode;
             }
+
+            $commandNode = new CommandNode();
+            $commandNode->parse($context);
+            $this->list[] = $commandNode;
         }
     }
 
